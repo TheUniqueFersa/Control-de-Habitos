8e13224dce1f7a273b1838885706f30f6b28b056
TheUniqueFersa <holafersa@gmail.com>
2023-11-21 00:01:29 -0600

    añadiendo librerias principales de C

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
 
 int main() {
     ALLEGRO_DISPLAY *disp;
@@ -7,10 +18,12 @@ int main() {
     if(!al_init()) {
         printf("No se pudo iniciar Allegro");
     }
-    disp = al_create_display(1200, 600);
+
+
+    disp = al_create_display(1200, 700);
     al_set_window_title(disp, "Hábitos");
 
-    for(i=0; i<=2500; i++){
+    for(i=0; i<=25000; i++){
         printf("\n%i", i);
     }
     al_destroy_display(disp);
